b8fea6f134b276031d73a1669dec25e2d7a9e7d7
liana <lianabub07@gmail.com>
2024-10-26 09:09:35 +0300

    Add files via upload

---
+++ lr3-2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+int main() {
+    setlocale(LC_ALL, "RU");
+    float a, b, c;
+    cout >> a >> b >> c;
+    if (a == b && b == c)
+    {
+
+        cout << "Этот треугольник равнобедренный ";
+    }
+    else if (a == b || b == c || c == a) {
+        cout << "Этот треугольник  равносторонний";
+    }
+    else if ((a + b < c)) or (a + c < a)) {
+        cout << "Этого треугольника не существует";
+         }
+    else if (pow(a, 2) == pow(b, 2) + pow(c, 2) || pow(b, 2) == pow(c, 2) || pow(b, 2) == pow(a, 2) + pow(c, 2)) {
+        cout << "Треугольник прямоугольный ";
+        exit;
+    }
+    else {
+        cout << "Треугольный равносторонний";
+    }
+    return 0;
+}
